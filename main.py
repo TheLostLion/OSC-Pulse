2e5805f075885663cab565647cbea54d5f4a3b00
Lost Lion <stianmag16@gmail.com>
2022-03-08 17:06:15 +0100

    added WebSocket

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+###########################################################
+# VRC OSC - Lost Lion
+###########################################################
 import random
 import time
 import threading
+import asyncio
+import websocket
+
+try:
+    import thread
+except ImportError:
+    import _thread as thread
 
 from pythonosc import udp_client
 from pythonosc import dispatcher
@@ -20,17 +30,62 @@ oscVRCServerPort = 9001
 ###########################################################
 
 
-
-
-
-# HpyeRate
+# HypeRate
 ###########################################################
 ###########################################################
+HRID = 4068
+Key = open("C:\\Users\Lost Lion\PycharmProjects\key.txt", "r")
+WebSocketKey = (Key.readline())
+Key.close()
+
+import time, json
 
 # put your HyperRate ID here
-# HypeRateID = ("https://app.hyperate.io/####")
+
+def StartWebsocket():
+    global webSocket
+    webSocket = websocket.WebSocketApp("wss://app.hyperate.io/socket/websocket?token=" + WebSocketKey,
+                                       on_message=on_message,
+                                       on_error=on_error,
+                                       on_close=on_close,
+                                       on_open=on_open)
+    webSocket.run_forever()
 
 
+def on_message(ws, message):
+    #print(message)
+    jsonPayload = json.loads(message)
+    if jsonPayload["event"] == "hr_update":
+        BPM = jsonPayload["payload"]["hr"]
+        #print("Current BPM: " + str(BPM))
+        client.send_message("/avatar/parameters/HeartBeat", (BPM / 200) * 2 - 1)
+
+
+def on_error(ws, error):
+    print(error)
+
+
+def on_close(ws):
+    print("### closed ###")
+
+
+def on_open(ws):
+    print("Opened websocket connection")
+    thread.start_new_thread(sendConnectionPayload, ())
+
+
+def sendConnectionPayload():
+    print("Sending connection payload")
+    webSocket.send('{"topic":"hr:' + str(HRID) + '","event":"phx_join","payload": {},"ref": 0}')
+    HeartbeatThread = threading.Thread(target=sendHeartbeat)
+    HeartbeatThread.start()
+
+
+def sendHeartbeat():
+    while True:
+        print("Sending heartbeat")
+        webSocket.send('{"topic": "phoenix","event": "heartbeat","payload": {},"ref": 0}')
+        time.sleep(30)
 
 
 ###########################################################
@@ -45,6 +100,7 @@ visemestore = []
 def muteHander(unused_addr, args, Muted):
 
     print("mute")
+    visemestore.clear()
 
     return
 
@@ -72,18 +128,29 @@ def visemeHandler(unused_addr, args, viseme):
         client.send_message("/avatar/parameters/Collar",False)
         visemestore.clear()
 
-    #HeartBeat detect
-    # will activate the HeartBeat  for 25 sec
-    if ("1 13 8 7"in visemeStream) or ("14 13 7 0" in visemeStream):
-        if random.randint(0, 100)> 50:
+    # HeartBeat detect
+    # will activate the HeartBeat  for 15 sec
+    if ("1 13 8 7" in visemeStream) or ("14 13 7 0" in visemeStream):
+        if random.randint(0, 100) > 50:
             print("HeartBeat")
         else:
             print("HeartBeat ooo")
         client.send_message("/avatar/parameters/HeartBeatShow", True)
-        time.sleep(25)
-        client.send_message("/avatar/parameters/HeartBeatShow",False)
+        time.sleep(15)
+        client.send_message("/avatar/parameters/HeartBeatShow", False)
         visemestore.clear()
 
+        # Blue detect
+        # will activate the Blue for 20 sec
+    if ("8 13 14 0" in visemeStream) or ("8 13 14 13" in visemeStream):
+        if random.randint(0, 100) > 50:
+            print("wow blue really ")
+        else:
+            print("green is better")
+        client.send_message("/avatar/parameters/Blue", True)
+        time.sleep(5)
+        client.send_message("/avatar/parameters/Blue", False)
+        visemestore.clear()
     return
 ###########################################################
 
